a87403916bfcf453c692dc8386331508c2b19128
Demarius Chrite <chrite.demarius@gmail.com>
2015-08-23 15:30:08 -0400

    More scope.

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ Vector.prototype.add = function (other) {
 
 Vector.prototype.subtract = function (other) {
 }
+
+Vector.prototype.multiply = function (s) {
+}
+
+Vector.prototype.equals = function (other) {
+}
+
+Vector.prototype.copy = function () {
+}
+
+Vector.prototype.max = function () {
+}
+
+Vector.prototype.modulus = function () {
+}
