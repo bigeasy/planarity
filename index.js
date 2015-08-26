9d1e3bd65ddbdc48374170e9fda912ac22e71214
Demarius Chrite <chrite.demarius@gmail.com>
2015-08-26 11:33:24 -0400

    Vector subtraction.

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ Vector.prototype.add = function (other) {
 }
 
 Vector.prototype.subtract = function (other) {
+    if (this.elems.length !== other.elems.length) return null;
+    return new Vector((function () {
+        var v = other.elems, ret = [];
+        for (var elem in v) {
+            ret.push(this.elems[elem] - v[elem])
+        }
+        return ret;
+    }).bind(this)())
 }
 
 Vector.prototype.multiply = function (s) {
