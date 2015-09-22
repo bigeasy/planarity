d70646322b9bd6f4a0d7b27f6e09df103039f069
Demarius Chrite <chrite.demarius@gmail.com>
2015-09-21 21:07:14 -0400

    Don't want these.

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ function Vector (list) { // :: [Int] -> Vector
 }
 
 Vector.prototype.add = function (other) { // :: Vector -> Vector
-    if (!(other instanceof Array)) return null;
-    if (this.elems.length !== other.elems.length) return null;
+    if (!(other instanceof Array)) return null
+    if (this.elems.length !== other.elems.length) return null
     return new Vector((function () {
-        var v = other.elems, ret = [];
+        var v = other.elems, ret = []
         for (var elem in v) {
             ret.push(v[elem] + this.elems[elem])
         }
-        return ret;
+        return ret
     }).bind(this)())
 }
 
 Vector.prototype.subtract = function (other) {
-    if (!(other instanceof Array)) return null;
-    if (this.elems.length !== other.elems.length) return null;
+    if (!(other instanceof Array)) return null
+    if (this.elems.length !== other.elems.length) return null
     return new Vector((function () {
-        var v = other.elems, ret = [];
+        var v = other.elems, ret = []
         for (var elem in v) {
             ret.push(this.elems[elem] - v[elem])
         }
-        return ret;
+        return ret
     }).bind(this)())
 }
 
 Vector.prototype.multiply = function (s) {
-    if (!(other instanceof Array)) return null;
-    if (this.elems.length !== other.elems.length) return null;
+    if (!(other instanceof Array)) return null
+    if (this.elems.length !== other.elems.length) return null
     for (var v in this.elems) {
         this.elems[v] = this.elems[v] * s
     }
@@ -39,35 +39,35 @@ Vector.prototype.multiply = function (s) {
 }
 
 Vector.prototype.equals = function (other) {
-    if (!(other instanceof Array)) return null;
-    if (this.elems.length !== other.elems.length) return false;
+    if (!(other instanceof Array)) return null
+    if (this.elems.length !== other.elems.length) return false
     for (var v in other.elems) {
-        if (other.elems[v] !== this.elems[v]) return false;
+        if (other.elems[v] !== this.elems[v]) return false
     }
-    return true;
+    return true
 }
 
 Vector.prototype.divide = function (other) {
-    if (!(other instanceof Array)) return null;
-    if (this.elems.length !== other.elems.length) return null;
+    if (!(other instanceof Array)) return null
+    if (this.elems.length !== other.elems.length) return null
 }
 
 Vector.prototype.copy = function () {
 }
 
 Vector.prototype.dot = function (other) {
-    if (!(other instanceof Array)) return null;
-    if (this.elems.length !== other.elems.length) return null;
-    var ret = 0;
+    if (!(other instanceof Array)) return null
+    if (this.elems.length !== other.elems.length) return null
+    var ret = 0
     for (var v in this.elems) {
         ret += this.elems[v] * other.elems[v]
     }
-    return ret;
+    return ret
 }
 
 Vector.prototype.cross = function (other) {
-    if (!(other instanceof Array)) return null;
-    if (this.elems.length !== other.elems.length) return null;
+    if (!(other instanceof Array)) return null
+    if (this.elems.length !== other.elems.length) return null
 }
 
 Vector.prototype.max = function () {
